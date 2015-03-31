a30089a472bb2eb35c460249dc56d86680d0f232
Andrew Gall <housepage@gmail.com>
2015-03-31 00:27:57 -0700

    Fix wage service

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -25,7 +25,8 @@ module.exports.metadata =
 {
     "org.apache.cordova.device": "0.2.13",
     "org.apache.cordova.console": "0.2.12",
-    "org.apache.cordova.statusbar": "0.1.9"
+    "org.apache.cordova.statusbar": "0.1.9",
+    "org.apache.cordova.geolocation": "0.3.12"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/js/AtWorkView.js
+++ platforms/browser/www/js/AtWorkView.js
@@ -4,12 +4,44 @@ var AtWorkView = function (answerService) {
       this.$el = $('<div/>');
 
       onGeolocate = function(loc) {
+
         answerService.saveAnswer("work-seattle","yes");
 
         var resultDiv = $(this.$el.find(".result")).html("In Seattle");
         var continueButton = $(this.$el.find("a.btn.hidden"));
         continueButton.removeClass("hidden");
       }
+
+      var geolocationDeferred = $.Deferred();
+
+      navigator.geolocation.getCurrentPosition(function(loc) {
+        geolocationDeferred.resolve(loc);
+      }, function() {
+        geolocationDeferred.reject("Error getting location");
+      });
+
+      var loadCityLimitsDeferred = $.Deferred();
+
+      $.ajax({
+          dataType: "json",
+          url: "data/city-limits.json",
+          success: function(cityLimits) {
+            loadCityLimitsDeferred.resolve(cityLimits);
+          },
+          error: function(response, status, errorThrown) {
+            loadCityLimitsDeferred.reject("Error loading city limits");
+          }
+      });
+
+      $.when(geolocationDeferred, loadCityLimitsDeferred).done( function(loc, cityLimits) {
+        var ww = Wherewolf();
+        ww.add("city-limits", cityLimits);
+
+        console.log(ww.find([loc.coords.latitude,loc.coords.longitude], {wholeFeature: true}));
+      }).fail( function(err1, err2) {
+        console.log(err1);
+        console.log(err2);
+      });
 
       setTimeout(onGeolocate.bind(this), 1000);
 
